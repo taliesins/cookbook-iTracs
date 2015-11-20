aea29aea8bfabc72d0eda95e1f0c2dadee2300bc
Taliesin Sisson <taliesins@interxion.com>
2015-11-20 16:20:00 +0000

    Compile script to exe and then install from exe

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,10 @@ directory working_directory do
 end
 
 itracs_install_script_path = File.join(working_directory, 'iTracsInstall.au3')
+itracs_install_exe_path = File.join(working_directory, 'iTracsInstall.exe')
 win_friendly_itracs_install_script_path = win_friendly_path(itracs_install_script_path)
+win_friendly_itracs_install_exe_path = win_friendly_path(itracs_install_exe_path)
+
 itracs_installed = is_package_installed?("#{node['itracs']['name']}")
 filename = File.basename(node['itracs']['url']).downcase
 download_path = File.join(working_directory, filename)
@@ -37,6 +40,11 @@ template itracs_install_script_path do
   not_if {itracs_installed}
 end
 
+execute "Compile #{win_friendly_itracs_install_script_path} with AutoIt" do
+  command "\"#{File.join(node['autoit']['home'], '/Aut2Exe/Aut2exe.exe')}\" /in \"#{win_friendly_itracs_install_script_path}\" /out \"#{win_friendly_itracs_install_exe_path}\""
+  not_if {itracs_installed}
+end
+
 remote_file download_path do
   source node['itracs']['url']
   checksum node['itracs']['checksum']
@@ -44,12 +52,14 @@ remote_file download_path do
 end
 
 execute "Install #{node['itracs']['name']} with AutoIt" do
-  command "\"#{File.join(node['autoit']['home'], 'AutoIt3.exe')}\" \"#{win_friendly_itracs_install_script_path}\""
+  command "\"#{win_friendly_itracs_install_exe_path}\""
   not_if {itracs_installed}
 end
 
 itracs_update_install_script_path = File.join(working_directory, 'iTracsUpgradeInstall.au3')
+itracs_update_install_exe_path = File.join(working_directory, 'iTracsUpgradeInstall.exe')
 win_friendly_itracs_update_install_script_path = win_friendly_path(itracs_update_install_script_path)
+win_friendly_itracs_update_install_exe_path = win_friendly_path(itracs_update_install_exe_path)
 update_directory = File.join(node['itracs']['properties']['TARGETDIR'], "PLM/Updater/_#{node['itracs']['update']['version']}")
 itracs_update_installed = ::File.directory?(update_directory)
 filename = File.basename(node['itracs']['update']['url']).downcase
@@ -60,6 +70,11 @@ template itracs_update_install_script_path do
   variables(
     WorkingDirectory: win_friendly_working_directory
   )
+  not_if {itracs_update_installed}
+end
+
+execute "Compile #{win_friendly_itracs_update_install_script_path} with AutoIt" do
+  command "\"#{File.join(node['autoit']['home'], '/Aut2Exe/Aut2exe.exe')}\" /in \"#{win_friendly_itracs_update_install_script_path}\" /out \"#{win_friendly_itracs_update_install_exe_path}\""
   not_if {itracs_update_installed}
 end
 
@@ -75,6 +90,6 @@ execute "Exract #{node['itracs']['name']} Update #{node['itracs']['update']['ver
 end
 
 execute "Install #{node['itracs']['name']} Update #{node['itracs']['update']['version']} with AutoIt" do
-  command "\"#{File.join(node['autoit']['home'], 'AutoIt3.exe')}\" \"#{win_friendly_itracs_update_install_script_path}\""
+  command "\"#{win_friendly_itracs_update_install_exe_path}\""
   only_if {itracs_update_installed}
 end
