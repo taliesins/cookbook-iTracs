ef4eaf0691bbb6c4108a0426b5742ec9f675a0f7
Taliesin Sisson <taliesins@interxion.com>
2015-12-01 15:09:30 +0000

    Changed the wrong file

--- recipes/default.rb
+++ recipes/default.rb
@@ -105,6 +105,6 @@ execute "Exract #{download_path} To #{win_friendly_working_directory}" do
 end
 
 execute "Install #{win_friendly_itracs_update_install_exe_path}" do
-  command "\"#{File.join(node['pstools']['home'], 'psexec.exe')}\" -accepteula -i \"#{win_friendly_itracs_update_install_exe_path}\""
+  command "\"#{File.join(node['pstools']['home'], 'psexec.exe')}\" -accepteula -i -s \"#{win_friendly_itracs_update_install_exe_path}\""
   not_if {itracs_update_installed}
 end
